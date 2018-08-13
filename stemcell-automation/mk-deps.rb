19a8070a3297cf1753fad037881f7fd820635f2d
Kenny DuMez <kdumez@pivotal.io>
2018-08-13 17:03:07 -0400

    Parse dependency sources for version & sha sources
    [#159661654](https://www.pivotaltracker.com/story/show/159661654)
    
    Signed-off-by: Simon Jones <sijones@pivotal.io>
    Co-authored-by: Simon Jones <sijones@pivotal.io>
    [#168997631] https://www.pivotaltracker.com/story/show/168997631 - move stemcell automation to stembuild (imported commit)

--- stemcell-automation/mk-deps.rb
+++ stemcell-automation/mk-deps.rb
@@ -1,13 +1,14 @@
- require 'digest'
- require 'json'
-
-*DEP_FILES = ARGV
+require 'digest'
+require 'json'
 
 depObj = {}
 
-DEP_FILES.each do |dep|
-  digest = Digest::SHA256.file(dep).hexdigest
-  depObj[dep] = {"sha"=> digest, "version" =>  ""}
+@dependencies = JSON.parse(File.read(ARGV[0])
+
+@dependencies.each do |dep|
+  digest = Digest::SHA256.file(dep[:file_source]).hexdigest
+  version = File.read(dep[:version_source]).chomp
+  depObj[dep] = {"sha"=> digest, "version" =>  version}
 end
 
 puts depObj.to_json
