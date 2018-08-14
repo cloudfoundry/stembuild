65b18f39fb7fbe230cae70b1072b6f0feeaec912
Ashwin Venkatesh <avenkatesh@pivotal.io>
2018-08-14 10:56:47 -0400

    Fixed bad ruby, added parens
    Co-authored-by: Matthew Horan <mhoran@pivotal.io>
    [#168997631] https://www.pivotaltracker.com/story/show/168997631 - move stemcell automation to stembuild (imported commit)

--- stemcell-automation/mk-deps.rb
+++ stemcell-automation/mk-deps.rb
@@ -3,7 +3,7 @@ require 'json'
 
 depObj = {}
 
-@dependencies = JSON.parse(File.read(ARGV[0])
+@dependencies = JSON.parse(File.read(ARGV[0]))
 
 @dependencies.each do |dep|
   digest = Digest::SHA256.file(dep[:file_source]).hexdigest
