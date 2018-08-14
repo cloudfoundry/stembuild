96b77a569ca345cced9004448b2cfca6d49c8cf3
Ashwin Venkatesh <avenkatesh@pivotal.io>
2018-08-14 11:07:52 -0400

    Changed symbols to strings
    Co-authored-by: Matthew Horan <mhoran@pivotal.io>
    [#168997631] https://www.pivotaltracker.com/story/show/168997631 - move stemcell automation to stembuild (imported commit)

--- stemcell-automation/mk-deps.rb
+++ stemcell-automation/mk-deps.rb
@@ -6,8 +6,8 @@ depObj = {}
 @dependencies = JSON.parse(File.read(ARGV[0]))
 
 @dependencies.each do |dep|
-  digest = Digest::SHA256.file(dep[:file_source]).hexdigest
-  version = File.read(dep[:version_source]).chomp
+  digest = Digest::SHA256.file(dep["file_source"]).hexdigest
+  version = File.read(dep["version_source"]).chomp
   depObj[dep] = {"sha"=> digest, "version" =>  version}
 end
 
