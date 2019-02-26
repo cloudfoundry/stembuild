b90653eb2a3690fe7924203efcffe3c10d7f8bb6
Gab Satchi <gsatchithanantham@pivotal.io>
2019-02-26 15:41:00 -0500

    Refactoring work for packager controller
    [#163993613]
    
    Co-authored-by: Garima Sharma <gasharma@pivotal.io>

--- package_stemcell/packagers/packager.go
+++ package_stemcell/packagers/packager.go
@@ -1,15 +1,38 @@
 package packagers
 
-import "io"
+import (
+	"fmt"
+	"io"
+)
 
 type Packager struct {
+	source Source
+	stemcellGenerator StemcellGenerator
 }
 
 //go:generate counterfeiter . Source
 type Source interface {
-	ArtifactReader() io.Reader
+	ArtifactReader() (io.Reader, error)
 }
+//go:generate counterfeiter . StemcellGenerator
+type StemcellGenerator interface {
+	Generate(reader io.Reader) error
+}
+
+func NewPackager(s Source, g StemcellGenerator) *Packager {
+	return &Packager{source: s, stemcellGenerator: g}
+}
+
 func (p *Packager) Package() error {
+	artifact, err := p.source.ArtifactReader()
+	if err != nil {
+		return fmt.Errorf("packager failed to retrieve artifact: %s", err)
+	}
+	err = p.stemcellGenerator.Generate(artifact)
+	if err != nil {
+		return fmt.Errorf("packager failed to generate stemcell: %s", err)
+	}
+
 	return nil
 }
 
--- package_stemcell/packagers/packager_test.go
+++ package_stemcell/packagers/packager_test.go
@@ -1,23 +1,69 @@
 package packagers_test
 
 import (
+	"bytes"
+	"errors"
 	"github.com/cloudfoundry-incubator/stembuild/package_stemcell/packagers"
+	"github.com/cloudfoundry-incubator/stembuild/package_stemcell/packagers/packagersfakes"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
 
 
 
-var _ = FDescribe("Packager", func() {
+var _ = Describe("Packager", func() {
 	Describe("Package", func() {
+
+		var (
+			source *packagersfakes.FakeSource
+			stemcellGenerator  *packagersfakes.FakeStemcellGenerator
+			packager *packagers.Packager
+		)
+
+
+		BeforeEach(func() {
+			source = &packagersfakes.FakeSource{}
+			stemcellGenerator = &packagersfakes.FakeStemcellGenerator{}
+			packager = packagers.NewPackager(source, stemcellGenerator)
+		})
+
 		It("doesn't return an error", func() {
-			packager := &packagers.Packager{}
 
 			err := packager.Package()
+
 			Expect(err).NotTo(HaveOccurred())
 		})
-		It("calls ArtifactReader method on source object", func(){
-			source := FakeSource
+
+		It("returns an error if ArtifactReader does", func() {
+
+			source.ArtifactReaderReturns(nil, errors.New("bad thing"))
+			err := packager.Package()
+
+			Expect(err).To(HaveOccurred())
+			Expect(err).To(MatchError("packager failed to retrieve artifact: bad thing"))
+		})
+
+		It("returns an error if Generate does", func() {
+
+			stemcellGenerator.GenerateReturns(errors.New("other bad thing"))
+
+			err := packager.Package()
+
+			Expect(err).To(MatchError("packager failed to generate stemcell: other bad thing"))
+		})
+
+		It("uses source object to generate stemcell", func(){
+			fakeIoReader := bytes.NewReader([]byte{})
+			source.ArtifactReaderReturns(fakeIoReader, nil)
+
+			packager.Package()
+
+			Expect(source.ArtifactReaderCallCount()).To(Equal(1))
+			Expect(stemcellGenerator.GenerateCallCount()).To(Equal(1))
+
+			argsForFirstCall := stemcellGenerator.GenerateArgsForCall(0)
+
+			Expect(argsForFirstCall).To(BeIdenticalTo(fakeIoReader))
 		})
 	})
 })
