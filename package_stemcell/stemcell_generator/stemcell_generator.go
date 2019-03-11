6806449a0153186e7243f7e380447fd434f8475b
Simon Jones <sijones@pivotal.io>
2019-03-11 15:03:39 -0400

    Contract testing mob session:   - tarWriter   - filenameGenerator [#163993613]

--- package_stemcell/stemcell_generator/stemcell_generator.go
+++ package_stemcell/stemcell_generator/stemcell_generator.go
@@ -4,18 +4,31 @@ import "io"
 
 //go:generate counterfeiter . ManifestGenerator
 type ManifestGenerator interface {
-	Generate(reader io.Reader) (io.Reader, error)
+	Manifest(reader io.Reader) (io.Reader, error)
 }
 
+//go:generate counterfeiter . FileNameGenerator
+type FileNameGenerator interface {
+	FileName() string
+}
+
+//go:generate counterfeiter . TarWriter
+type TarWriter interface {
+	Write(string, ...io.Reader)
+}
 type StemcellGenerator struct {
 	manifestGenerator ManifestGenerator
+	fileNameGenerator FileNameGenerator
+	tarWriter TarWriter
 }
 
-func NewStemcellGenerator(m ManifestGenerator) *StemcellGenerator {
-	return &StemcellGenerator{m}
+func NewStemcellGenerator(m ManifestGenerator, f FileNameGenerator, t TarWriter) *StemcellGenerator {
+	return &StemcellGenerator{m, f, t}
 }
 
 func (g *StemcellGenerator) Generate(image io.Reader) error {
-	g.manifestGenerator.Generate(image)
+	manifest, _ := g.manifestGenerator.Manifest(image)
+	filename := g.fileNameGenerator.FileName()
+	g.tarWriter.Write(filename, image, manifest)
 	return nil
 }
--- package_stemcell/stemcell_generator/stemcell_generator_test.go
+++ package_stemcell/stemcell_generator/stemcell_generator_test.go
@@ -3,7 +3,7 @@ package stemcell_generator_test
 import (
 	"bytes"
 	"github.com/cloudfoundry-incubator/stembuild/package_stemcell/stemcell_generator"
-	"github.com/cloudfoundry-incubator/stembuild/package_stemcell/stemcell_generator/generatorfakes"
+	fakes "github.com/cloudfoundry-incubator/stembuild/package_stemcell/stemcell_generator/stemcell_generatorfakes"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"io"
@@ -13,24 +13,51 @@ var _ = Describe("StemcellGenerator", func() {
 	Describe("Generate", func() {
 		var (
 			stemcellGenerator *stemcell_generator.StemcellGenerator
-			manifestGenerator *generatorfakes.FakeManifestGenerator
+			manifestGenerator *fakes.FakeManifestGenerator
+			fileNameGenerator *fakes.FakeFileNameGenerator
+			tarWriter *fakes.FakeTarWriter
 			fakeImage         io.Reader
 		)
 
 		BeforeEach(func() {
 			fakeImage = bytes.NewReader([]byte{})
-			manifestGenerator = &generatorfakes.FakeManifestGenerator{}
-			stemcellGenerator = stemcell_generator.NewStemcellGenerator(manifestGenerator)
+			manifestGenerator = &fakes.FakeManifestGenerator{}
+			fileNameGenerator = &fakes.FakeFileNameGenerator{}
+			tarWriter = &fakes.FakeTarWriter{}
+			stemcellGenerator = stemcell_generator.NewStemcellGenerator(manifestGenerator, fileNameGenerator, tarWriter)
 		})
 
 		It("generates a manifest", func() {
 			err := stemcellGenerator.Generate(fakeImage)
 
 			Expect(err).NotTo(HaveOccurred())
-			Expect(manifestGenerator.GenerateCallCount()).To(Equal(1))
+			Expect(manifestGenerator.ManifestCallCount()).To(Equal(1))
 
-			args := manifestGenerator.GenerateArgsForCall(0)
+			args := manifestGenerator.ManifestArgsForCall(0)
 			Expect(args).To(Equal(fakeImage))
+		})
+
+		It("generates a filename", func() {
+			err := stemcellGenerator.Generate(fakeImage)
+
+			Expect(err).NotTo(HaveOccurred())
+			Expect(fileNameGenerator.FileNameCallCount()).To(Equal(1))
+		})
+		It("should generate a tarball", func(){
+			expectedFileName := "the-file.tgz"
+			expectedManifest := bytes.NewReader([]byte("manifest"))
+
+			fileNameGenerator.FileNameReturns(expectedFileName)
+			manifestGenerator.ManifestReturns(expectedManifest, nil)
+
+			stemcellGenerator.Generate(fakeImage)
+			Expect(tarWriter.WriteCallCount()).To(Equal(1))
+
+			actualFileName, objects := tarWriter.WriteArgsForCall(0)
+
+			Expect(actualFileName).To(Equal(expectedFileName))
+
+			Expect(objects).To(ConsistOf(expectedManifest, fakeImage))
 		})
 
 	})
