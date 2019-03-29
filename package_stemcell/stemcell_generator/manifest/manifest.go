84606eec4a1570658bfdd5d021874dae18317c27
Jason Smith <jsmith@pivotal.io>
2019-03-29 10:07:52 -0400

    Initial manifest generator test
    [#163993613]
    
    Co-authored-by: Simon Jones <sijones@pivotal.io>

--- package_stemcell/stemcell_generator/manifest/manifest.go
+++ package_stemcell/stemcell_generator/manifest/manifest.go
@@ -2,11 +2,38 @@ package manifest
 
 import (
 	"bytes"
+	"crypto/sha1"
+	"fmt"
 	"io"
 )
 
-type ManifestGenerator struct{}
+type ManifestGenerator struct {
+	os      string
+	version string
+}
 
-func (m *ManifestGenerator) Manifest(reader io.Reader) (io.Reader, error) {
-	return bytes.NewReader([]byte("Im not nil")), nil
+func NewManifestGenerator(os, version string) *ManifestGenerator {
+	return &ManifestGenerator{os, version}
+}
+
+func (m *ManifestGenerator) Manifest(image io.Reader) (io.Reader, error) {
+	const manifestTemplate = `---
+name: bosh-vsphere-esxi-windows%[1]s-go_agent
+version: '%[2]s'
+sha1: '%[3]x'
+operating_system: windows%[1]s
+cloud_properties:
+  infrastructure: vsphere
+  hypervisor: esxi
+stemcell_formats:
+- vsphere-ovf
+- vsphere-ova
+`
+	sha1Hash := sha1.New()
+	io.Copy(sha1Hash, image)
+
+	sum := sha1Hash.Sum(nil)
+
+	manifestContent := fmt.Sprintf(manifestTemplate, m.os, m.version, sum)
+	return bytes.NewReader([]byte(manifestContent)), nil
 }
--- package_stemcell/stemcell_generator/manifest/manifest_test.go
+++ package_stemcell/stemcell_generator/manifest/manifest_test.go
@@ -2,21 +2,46 @@ package manifest_test
 
 import (
 	"bytes"
+	"fmt"
+
 	"github.com/cloudfoundry-incubator/stembuild/package_stemcell/stemcell_generator/manifest"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
 
 var _ = Describe("Manifest", func() {
+
+	const format = `---
+name: bosh-vsphere-esxi-windows%[1]s-go_agent
+version: '%[2]s'
+sha1: '%[3]s'
+operating_system: windows%[1]s
+cloud_properties:
+  infrastructure: vsphere
+  hypervisor: esxi
+stemcell_formats:
+- vsphere-ovf
+- vsphere-ova
+`
 	Describe("Manifest", func() {
 		It("should return a manifest", func() {
-			manifestGenerator := &manifest.ManifestGenerator{}
+			manifestGenerator := manifest.NewManifestGenerator("1709", "1709.999")
 			fakeImage := bytes.NewReader([]byte("An image"))
 
 			manifest, err := manifestGenerator.Manifest(fakeImage)
 
 			Expect(err).ToNot(HaveOccurred())
-			Expect(manifest).NotTo(BeNil())
+
+			buf := new(bytes.Buffer)
+			_, err = buf.ReadFrom(manifest)
+			Expect(err).NotTo(HaveOccurred())
+			s := buf.String()
+
+			//output of `echo "An image" | shasum`
+			// TODO: work out where this shasum comes from
+			shaSum := "bf8a473a2baa3988b4e7fc4702c35303cdf6df6b"
+
+			Expect(s).To(Equal(fmt.Sprintf(format, "1709", "1709.999", shaSum)))
 		})
 	})
 })
