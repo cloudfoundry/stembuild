a6c4678f5d4acce637e0f547b4bd04d2cd2945f5
Austina Lin <aulin@pivotal.io>
2020-04-27 15:23:23 -0400

    add test coverage for CanLoginVM
    also, ran dep ensure which updated some dependencies
    
    Co-authored-by: George Gelashvili <ggelashvili+cf@pivotal.io>
    Co-authored-by: Anand Gaitonde <agaitonde@pivotal.io>
    Co-authored-by: Frankie Gallina-Jones <fgallinajones@pivotal.io>
    
    [#172546049](https://www.pivotaltracker.com/story/show/172546049)
     We should have test coverage for CanLoginVM

--- remotemanager/winrm_remotemanager_test.go
+++ remotemanager/winrm_remotemanager_test.go
@@ -1,12 +1,50 @@
 package remotemanager_test
 
 import (
+	"bytes"
 	"errors"
+	"fmt"
 	"github.com/cloudfoundry-incubator/stembuild/remotemanager"
 	"github.com/cloudfoundry-incubator/stembuild/remotemanager/remotemanagerfakes"
+	"github.com/masterzen/winrm"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	. "github.com/onsi/gomega/ghttp"
+	"log"
+	"net/http"
+	"net/url"
+	"strconv"
 )
+
+////go:generate counterfeiter . FakeShell
+//type FakeShell interface {
+//	 Close() error
+//}
+
+func setupTestServer() *Server {
+	server := NewServer()
+
+	// Suppresses ginkgo server logs
+	server.HTTPTestServer.Config.ErrorLog = log.New(&bytes.Buffer{}, "", 0)
+
+	response := `
+<s:Envelope xmlns:s="https://www.w3.org/2003/05/soap-envelope" 
+            xmlns:a="https://schemas.xmlsoap.org/ws/2004/08/addressing" 
+            xmlns:w="https://schemas.dmtf.org/wbem/wsman/1/wsman.xsd">
+  <s:Header>
+    <w:ShellId>153600</w:ShellId>
+  </s:Header>
+  <s:Body/> 
+</s:Envelope>
+` // Looks for: //w:Selector[@Name='ShellId']
+	server.AppendHandlers(
+		RespondWith(http.StatusOK, response, http.Header{
+			"Content-Type": {"application/soap+xml;charset=UTF-8"},
+		}),
+	)
+	server.AllowUnhandledRequests = true
+	return server
+}
 
 var _ = Describe("WinRM RemoteManager", func() {
 	Describe("ExecuteCommand", func() {
@@ -81,5 +119,67 @@ var _ = Describe("WinRM RemoteManager", func() {
 			})
 		})
 
+	})
+	Describe("CanLoginVM", func() {
+		var (
+			testServer  *Server
+			winRMClient *winrm.Client
+		)
+
+		BeforeEach(func() {
+			testServer = setupTestServer()
+
+			testServerURL, err := url.Parse(testServer.URL())
+			Expect(err).NotTo(HaveOccurred())
+			port, err := strconv.Atoi(testServerURL.Port())
+			Expect(err).NotTo(HaveOccurred())
+
+			endpoint := &winrm.Endpoint{
+				Host: testServerURL.Hostname(),
+				Port: port,
+			}
+			winRMClient, err = winrm.NewClient(endpoint, "", "")
+			Expect(err).NotTo(HaveOccurred())
+		})
+
+		var _ = AfterEach(func() {
+			testServer.Close()
+		})
+
+		It("returns nil if shell can be created", func() {
+			winRMClientFactory := new(remotemanagerfakes.FakeWinRMClientFactoryI)
+			winRMClientFactory.BuildReturns(winRMClient, nil)
+
+			remotemanager := remotemanager.NewWinRM("some-host", "some-user", "some-pass", winRMClientFactory)
+
+			err := remotemanager.CanLoginVM()
+
+			Expect(err).NotTo(HaveOccurred())
+		})
+		It("returns error if winrmclient cannot be created", func() {
+			winRMClientFactory := new(remotemanagerfakes.FakeWinRMClientFactoryI)
+			buildError := errors.New("unable to build a client")
+			winRMClientFactory.BuildReturns(nil, buildError)
+
+			remotemanager := remotemanager.NewWinRM("some-host", "some-user", "some-pass", winRMClientFactory)
+			err := remotemanager.CanLoginVM()
+
+			Expect(err).To(HaveOccurred())
+			Expect(err).To(MatchError(fmt.Errorf("failed to create winrm client: %s", buildError)))
+		})
+		It("returns error if shell cannot be created", func() {
+			winRMClientFactory := new(remotemanagerfakes.FakeWinRMClientFactoryI)
+			winRMClient := new(remotemanagerfakes.FakeWinRMClient)
+			winRMClientFactory.BuildReturns(winRMClient, nil)
+
+			winRMClient.CreateShellReturns(nil, errors.New("some shell creation error"))
+			remotemanager := remotemanager.NewWinRM("some-host", "some-user", "some-pass", winRMClientFactory)
+
+			err := remotemanager.CanLoginVM()
+
+			Expect(err).To(HaveOccurred())
+			Expect(err).To(MatchError(fmt.Errorf("failed to create winrm shell: some shell creation error")))
+
+		})
 	})
 })
