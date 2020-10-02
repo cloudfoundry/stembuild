060cca6e4d7948c3f340ef952047fc63498239c3
Anand Gaitonde <agaitonde@pivotal.io>
2020-10-02 11:31:53 -0700

    split integration test into two due to output ordering
    Co-authored-by: George Gelashvili <ggelashvili+cf@pivotal.io>
    Co-authored-by: Sam Coward <scoward@pivotal.io>

--- integration/environment_variable_warnings_test.go
+++ integration/environment_variable_warnings_test.go
@@ -19,10 +19,15 @@ var _ = Describe("Environment Variables Warning", func() {
 	})
 
 	When("Environment variables starting with GOVC_* or GOVMOMI_* are set", func() {
-		It("should print a warning for each environment variable", func() {
+		It("should print a warning for each GOVC_* environment variable", func() {
 			session := helpers.StembuildWithEnv(map[string]string{"GOVC_BANANA": "I AM A BANANA", "GOVMOMI_POTATO": "THIS IS A POTATO"}, stembuildExecutable, "--version")
 			Eventually(session.Err).Should(Say("Warning: The following environment variable is set and might override flags provided to stembuild: GOVC_BANANA\n"))
 			Consistently(session.Err).ShouldNot(Say("I AM A BANANA"))
+			Eventually(session).Should(Exit(0))
+		})
+
+		It("should print a warning for each GOVMOMI_* environment variable", func() {
+			session := helpers.StembuildWithEnv(map[string]string{"GOVC_BANANA": "I AM A BANANA", "GOVMOMI_POTATO": "THIS IS A POTATO"}, stembuildExecutable, "--version")
 			Eventually(session.Err).Should(Say("Warning: The following environment variable is set and might override flags provided to stembuild: GOVMOMI_POTATO\n"))
 			Consistently(session.Err).ShouldNot(Say("THIS IS A POTATO"))
 			Eventually(session).Should(Exit(0))
