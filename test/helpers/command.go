6cb2d1593145c4bf0e38e3e29ca06d22ff2e1082
aram price <pricear@vmware.com>
2023-06-08 10:15:37 -0700

    golangci-lint: add `nolint` directive

--- test/helpers/command.go
+++ test/helpers/command.go
@@ -55,7 +55,7 @@ func StembuildWithEnv(passedEnv map[string]string, command string, args ...strin
 
 func WriteCommand(command string, args []string) {
 	display := append([]string{DebugCommandPrefix, command}, args...)
-	GinkgoWriter.Write([]byte(strings.Join(append(display, "\n"), " ")))
+	GinkgoWriter.Write([]byte(strings.Join(append(display, "\n"), " "))) //nolint:errcheck
 }
 
 // AddOrReplaceEnvironment will update environment if it already exists or will add
