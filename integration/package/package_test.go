41ee6e3f40240488bb6714ffc23d983175f6ba1e
aram price <pricear@vmware.com>
2023-06-07 17:17:48 -0700

    Revert error check in specs

--- integration/package/package_test.go
+++ integration/package/package_test.go
@@ -88,7 +88,7 @@ var _ = Describe("Package", func() {
 
 		Eventually(session, 60*time.Minute, 5*time.Second).Should(gexec.Exit(0))
 		var out []byte
-		Expect(session.Out.Write(out)).To(Succeed())
+		session.Out.Write(out)
 		By(string(out))
 
 		expectedOSVersion := strings.Split(stembuildVersion, ".")[0]
@@ -143,7 +143,7 @@ var _ = Describe("Package", func() {
 
 		Eventually(session, 60*time.Minute, 5*time.Second).Should(gexec.Exit(0))
 		var out []byte
-		Expect(session.Out.Write(out)).To(Succeed())
+		session.Out.Write(out)
 		By(string(out))
 
 		expectedOSVersion := strings.Split(stembuildVersion, ".")[0]
