724200c466bf48f3dbebdb3266df93c25a9fda31
Shawn Neal <sneal@sneal.net>
2024-06-24 20:18:55 -0700

    Issue: #27
    Add rollback of "automatic retry on timeout" in winrm when executing the post reboot script. This should fix the hang reported during sysprep.

--- remotemanager/winrm_clientfactory.go
+++ remotemanager/winrm_clientfactory.go
@@ -18,6 +18,12 @@ func NewWinRmClientFactory(host, username, password string) *WinRMClientFactory 
 
 func (f *WinRMClientFactory) Build(timeout time.Duration) (WinRMClient, error) {
 	endpoint := winrm.NewEndpoint(f.host, WinRmPort, false, true, nil, nil, nil, timeout)
-	client, err := winrm.NewClient(endpoint, f.username, f.password)
+	params := winrm.NewParameters(
+		winrm.DefaultParameters.Timeout,
+		winrm.DefaultParameters.Locale,
+		winrm.DefaultParameters.EnvelopeSize,
+	)
+	params.AllowTimeout = true
+	client, err := winrm.NewClientWithParameters(endpoint, f.username, f.password, params)
 	return client, err
 }
