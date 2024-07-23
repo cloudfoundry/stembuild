4479c6d91791b3afb069f088d1093b4e250e23e2
aram price <aram.price@broadcom.com>
2024-07-23 16:12:05 -0700

    colorlogger package looks more like log.Logger

--- colorlogger/colorLogger.go
+++ colorlogger/colorLogger.go
@@ -6,38 +6,44 @@ import (
 	"log"
 )
 
+const (
+	NONE  = -1
+	DEBUG = 0
+
+	debug             = "debug"
+	plainPrefixFormat = "%s:"
+	colorPrefixFormat = "\033[32m%s:\033[0m"
+)
+
 type colorLogger struct {
 	color    bool
 	logger   *log.Logger
 	logLevel int
 }
 
-const (
-	NONE  = -1
-	DEBUG = 0
-)
-
-var logLevelsNames = []string{"debug"}
-
-func ConstructLogger(logLevel int, color bool, writer io.Writer) *colorLogger {
-	logger := log.New(writer, "", 0)
-	loggerPlus := colorLogger{color, logger, logLevel}
-	return &loggerPlus
+type Logger interface {
+	Printf(format string, args ...interface{})
 }
 
-func (g *colorLogger) Logf(logLevel int, msg string) {
-	if g.logLevel >= logLevel && g.logLevel != NONE {
-		if g.color {
-			g.logger.Printf("\033[32m%s:\033[0m %s", logLevelsNames[logLevel], msg)
-		} else {
-			g.logger.Printf("%s: %s", logLevelsNames[logLevel], msg)
-
-		}
+func New(logLevel int, color bool, writer io.Writer) Logger {
+	return &colorLogger{
+		color:    color,
+		logger:   log.New(writer, "", 0),
+		logLevel: logLevel,
 	}
 }
 
-// This function is here so that existing Debugf outputs will still work.
-// Once we figure out how to properly deal with logging, this can be revisited
-func (g *colorLogger) Debugf(format string, a ...interface{}) {
-	g.Logf(DEBUG, fmt.Sprintf(format, a...))
+func (cl *colorLogger) Printf(format string, a ...interface{}) {
+	if cl.logLevel >= DEBUG && cl.logLevel != NONE {
+		cl.logger.Printf("%s %s", cl.prefix(), fmt.Sprintf(format, a...))
+	}
+}
+
+func (cl *colorLogger) prefix() string {
+	prefixFormat := plainPrefixFormat
+	if cl.color {
+		prefixFormat = colorPrefixFormat
+	}
+
+	return fmt.Sprintf(prefixFormat, debug)
 }
--- colorlogger/colorlogger_test.go
+++ colorlogger/colorlogger_test.go
@@ -10,50 +10,37 @@ import (
 )
 
 var _ = Describe("Stdout", func() {
-
 	It("write debug output when log level is debug", func() {
 		buf := bytes.Buffer{}
 
 		Expect(colorlogger.DEBUG).To(Equal(0))
-		logger := colorlogger.ConstructLogger(colorlogger.DEBUG, false, &buf)
+		logger := colorlogger.New(colorlogger.DEBUG, false, &buf)
 
 		message := "This is a test"
 
-		logger.Logf(colorlogger.DEBUG, message)
+		logger.Printf(message)
 		Expect(buf.String()).To(Equal("debug: " + message + "\n"))
 	})
 
 	It("write no debug output when log level is NONE", func() {
 		buf := bytes.Buffer{}
 
-		logger := colorlogger.ConstructLogger(colorlogger.NONE, false, &buf)
+		logger := colorlogger.New(colorlogger.NONE, false, &buf)
 
 		message := "This is a test"
 
-		logger.Logf(colorlogger.DEBUG, message)
-		Expect(buf.String()).To(BeEmpty())
-	})
-
-	It("write no none output when log level is NONE", func() {
-		buf := bytes.Buffer{}
-
-		logger := colorlogger.ConstructLogger(colorlogger.NONE, false, &buf)
-
-		message := "This is a test"
-
-		logger.Logf(colorlogger.NONE, message)
+		logger.Printf(message)
 		Expect(buf.String()).To(BeEmpty())
 	})
 
 	It("write colored debug output when log level is DEBUG and color is true", func() {
 		buf := bytes.Buffer{}
 
-		logger := colorlogger.ConstructLogger(colorlogger.DEBUG, true, &buf)
+		logger := colorlogger.New(colorlogger.DEBUG, true, &buf)
 
 		message := "This is a test"
 
-		logger.Logf(colorlogger.DEBUG, message)
+		logger.Printf(message)
 		Expect(buf.String()).To(Equal("\033[32mdebug:\033[0m " + message + "\n"))
 	})
-
 })
