993c17522d89549a697db39c50ec0218010784cc
aram price <aram.price@broadcom.com>
2025-05-01 16:56:51 -0700

    Relocate stembuild into stembuild/ subdir
    This work is in preparation for consolidating this repo into
    bosh-windows-stemcell-builder.

--- bin/build-stemcell-automation-zip.sh
+++ bin/build-stemcell-automation-zip.sh
@@ -4,10 +4,10 @@ set -eu -o pipefail
 ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
 STEMCELL_AUTOMATION_PS1=$(ls "${ROOT_DIR}"/stemcell-automation/*ps1 | grep -iv Test)
 
-: ${OPENSSH_ZIP?"Please see README.md on where to obtain this."}
-: ${BOSH_PSMODULES_ZIP?"Please see README.md on where to obtain this."}
-: ${AGENT_ZIP?"Please see README.md on how to construct this."}
-: ${DEPS_JSON?"Please see README.md on how to construct this."}
+: "${OPENSSH_ZIP?"Please see README.md on where to obtain this."}"
+: "${BOSH_PSMODULES_ZIP?"Please see README.md on where to obtain this."}"
+: "${AGENT_ZIP?"Please see README.md on how to construct this."}"
+: "${DEPS_JSON?"Please see README.md on how to construct this."}"
 
 TEMP_DIR=$(mktemp -d)
 
@@ -15,10 +15,10 @@ cp "${OPENSSH_ZIP}" "${TEMP_DIR}/OpenSSH-Win64.zip"
 cp "${BOSH_PSMODULES_ZIP}" "${TEMP_DIR}/bosh-psmodules.zip"
 cp "${AGENT_ZIP}" "${TEMP_DIR}/agent.zip"
 cp "${DEPS_JSON}" "${TEMP_DIR}/deps.json"
-cp ${STEMCELL_AUTOMATION_PS1} "$TEMP_DIR"
+cp "${STEMCELL_AUTOMATION_PS1}" "${TEMP_DIR}"
 
-rm -f "${ROOT_DIR}/assets/StemcellAutomation.zip"
+rm -f "${ROOT_DIR}/stembuild/assets/StemcellAutomation.zip"
 
-zip -rj "${ROOT_DIR}/assets/StemcellAutomation.zip" "$TEMP_DIR"
+zip -rj "${ROOT_DIR}/stembuild/assets/StemcellAutomation.zip" "${TEMP_DIR}"
 
-rm -r "$TEMP_DIR"
+rm -r "${TEMP_DIR}"
--- commandparser/export_construct_test.go
+++
@@ -1,7 +0,0 @@
-package commandparser
-
-import "github.com/cloudfoundry/stembuild/construct/config"
-
-func (p *ConstructCmd) GetSourceConfig() config.SourceConfig {
-	return p.sourceConfig
-}
