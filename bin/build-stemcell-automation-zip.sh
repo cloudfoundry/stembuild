e95b64b4bbbdc24c5507fd4e86441e1259bacb06
Aram Price <aram.price@broadcom.com>
2025-05-07 15:03:01 -0700

    Loop through PS1 files instead of globbing
    Signed-off-by: Brian Upton <brian.upton@broadcom.com>

--- bin/build-stemcell-automation-zip.sh
+++ bin/build-stemcell-automation-zip.sh
@@ -2,7 +2,6 @@
 set -eu -o pipefail
 
 REPO_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
-STEMCELL_AUTOMATION_PS1=$(ls "${REPO_ROOT}"/stembuild/stemcell-automation/*ps1 | grep -iv Test)
 
 : "${OPENSSH_ZIP?"Please see README.md on where to obtain this."}"
 : "${BOSH_PSMODULES_ZIP?"Please see README.md on where to obtain this."}"
@@ -15,7 +14,9 @@ cp "${OPENSSH_ZIP}" "${TEMP_DIR}/OpenSSH-Win64.zip"
 cp "${BOSH_PSMODULES_ZIP}" "${TEMP_DIR}/bosh-psmodules.zip"
 cp "${AGENT_ZIP}" "${TEMP_DIR}/agent.zip"
 cp "${DEPS_JSON}" "${TEMP_DIR}/deps.json"
-cp "${STEMCELL_AUTOMATION_PS1}" "${TEMP_DIR}"
+for file in $(ls "${REPO_ROOT}"/stembuild/stemcell-automation/*ps1 | grep -iv Test); do
+  cp "${file}" "${TEMP_DIR}"
+done
 
 rm -f "${REPO_ROOT}/stembuild/assets/StemcellAutomation.zip"
 
